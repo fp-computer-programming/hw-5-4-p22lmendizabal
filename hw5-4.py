83258f6e238c74cde2080397419b0ed5f56190f3
p22mendizabal <p22lmendizabal@fairfieldprep.org>
2021-10-30 22:26:08 -0400

    add file

---
+++ hw5-4.py
@@ -0,0 +1,22 @@
+# author: LM (AMDG) 10/29/21
+# Question 1
+print("Python" [2])
+# Question 2
+print("Strings are simply a sequence of \'o\' characters.")
+# Question 3
+print(len('Excellent'))
+# Question 4
+print("apple" in "Pineapple")
+# Question 5
+print("Peach" > "pear")
+# Question 6
+print("x" * 100)
+# Question 7
+print(str(54321))
+# Question 8
+print("p" not in "Pineapple")
+# Question 9
+print('"Livin\' On a Prayer" is my friend\'s favorite song.')
+# Question 10
+
+print(len('"Livin\' On a Prayer" is my friend\'s favorite song.'))
